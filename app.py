1c9f9d01cf684afe69ab9a008858eacd34282756
710coud710 <dduoc2002@gmail.com>
2025-03-06 15:01:05 +0700

    update7.5

--- app.py
+++ app.py
@@ -80,6 +80,6 @@ def get_random_user():
     return jsonify(generate_fake_data())
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000)) 
-    # app.run(host="0.0.0.0", port=port, debug=True) 
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True) 
+    # app.run(host="0.0.0.0", port=5000, debug=True)
